03e0f2a5b8aa8ba74e9273664e9ac41cbda0b445
Koji Hasegawa <hasegawa@hubsys.co.jp>
2012-08-04 01:20:25 +0900

    Fix broken popover frame, on TSPopoverArrowDirectionBottom

--- TSPopover/TSPopoverController.m
+++ TSPopover/TSPopoverController.m
@@ -252,6 +252,9 @@
     contentFrameRect.origin.y = MARGIN;
     
     float statusBarHeight = [[UIApplication sharedApplication] statusBarFrame].size.height;
+    if(self.interfaceOrientation == UIInterfaceOrientationLandscapeLeft || self.interfaceOrientation == UIInterfaceOrientationLandscapeRight){
+        statusBarHeight = [[UIApplication sharedApplication] statusBarFrame].size.width;
+    }
 
 
     if(self.arrowPosition == TSPopoverArrowPositionVertical){
